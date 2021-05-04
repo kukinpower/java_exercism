4b9288708ed2d97e7ce494b8092477c47218e9ee
kukinpower <thekukinpower@gmail.com>
2021-05-04 15:21:34 +0300

    using analytical formula in difference-of-squares

--- difference-of-squares/src/main/java/DifferenceOfSquaresCalculator.java
+++ difference-of-squares/src/main/java/DifferenceOfSquaresCalculator.java
@@ -1,18 +1,15 @@
-import java.util.stream.IntStream;
-
 class DifferenceOfSquaresCalculator {
 
-    int computeSquareOfSumTo(int input) {
-        int sum = IntStream.rangeClosed(1, input).sum();
-        return sum * sum;
-    }
+  int computeSquareOfSumTo(int input) {
+    return (int) Math.pow(input * (input + 1) / 2, 2);
+  }
 
-    int computeSumOfSquaresTo(int input) {
-        return IntStream.rangeClosed(1, input).map(a -> a * a).sum();
-    }
+  int computeSumOfSquaresTo(int input) {
+    return input * (input + 1) * (2 * input + 1) / 6;
+  }
 
-    int computeDifferenceOfSquares(int input) {
-        return Math.abs(computeSumOfSquaresTo(input) - computeSquareOfSumTo(input));
-    }
+  int computeDifferenceOfSquares(int input) {
+    return Math.abs(computeSumOfSquaresTo(input) - computeSquareOfSumTo(input));
+  }
 
 }
